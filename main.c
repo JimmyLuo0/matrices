33e5b548ddd119cc91ad6d3f324ce6ac1e6c8d60
JimmyLuo0 <jluo00@stuy.edu>
2020-02-13 11:38:42 -0500

    identity matrix

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+#include "matrix.h"
+
+int main() {
+
+  screen s;
+  struct matrix *edges;
+
+  edges = new_matrix(4, 4);
+  printf("4 by 4 of zeroes\n");
+  print_matrix(edges);
+
+
+  ident(edges);
+  printf("4 by 4 identity matrix\n");
+  print_matrix(edges);
+  free_matrix( edges );
+}
